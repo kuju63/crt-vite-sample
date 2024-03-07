fa9f371c8c1459da377275171c8d98f8483195ad
kuju63 <lh182051+src@gmail.com>
2024-03-07 23:12:01 +0900

    initial commit

---
+++ vite/src/pages/top/TopPage.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../app/store";
+import TopPage from "./TopPage";
+
+test("renders learn react link", () => {
+  render(
+    <Provider store={store}>
+      <TopPage />
+    </Provider>
+  );
+
+  expect(screen.getByText(/learn/i)).toBeInTheDocument();
+});
